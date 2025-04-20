ec55bae528e9314b0891102f7e6290309669ba02
mizazer <121462420+mizazer@users.noreply.github.com>
2025-04-20 22:19:17 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # رابط البث المباشر (مثال: SSC)
-SOURCE="https://watch.3rbcafee.com/2024/10/sscnew-prem.html?id=SSC1"
+SOURCE="https://www.elahmad.com/tv/live/shahid_shaka.php?id=ssc1"
 
 # رابط الإرسال (مثال: YouTube, Facebook, إلخ)
 # لازم تغيّر هذا حسب اللي تحب تبث فيه
