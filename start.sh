e3f5d015c7da6fd334048d960b4cfae281b988f2
mizazer <121462420+mizazer@users.noreply.github.com>
2025-04-20 22:22:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ SOURCE="https://www.elahmad.com/tv/live/shahid_shaka.php?id=ssc1"
 
 # رابط الإرسال (مثال: YouTube, Facebook, إلخ)
 # لازم تغيّر هذا حسب اللي تحب تبث فيه
-DEST="rtmp://live.restream.io/live/YOUR_STREAM_KEY"
+DEST="rtmp://live.restream.io/studio/streamv2_9518871_145adf010235485884ae41c78f59e5bb6d8efb"
 
 # ffmpeg command
 ffmpeg -re -i "$SOURCE" -c:v copy -c:a aac -f flv "$DEST"
