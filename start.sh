b36adf453b22755d73b97974946a1019a13ef176
mizazer <121462420+mizazer@users.noreply.github.com>
2025-04-21 12:49:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # رابط البث المباشر (مثال: SSC)
-SOURCE="https://dd.alkoora.live/albaplayer/ad-sport-1"
+SOURCE="https://live-hls-abr-cdn.livepush.io/live/bigbuckbunnyclip/index.m3u8"
 
 # رابط الإرسال (مثال: YouTube, Facebook, إلخ)
 # لازم تغيّر هذا حسب اللي تحب تبث فيه
